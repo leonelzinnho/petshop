661258531b308a7eaf220f340bc500954afa47eb
ggfrancagg <gfrancagg@gmail.com>
2023-05-26 17:14:21 -0300

    função logar

--- pages/cabecalho.php
+++ pages/cabecalho.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sistema PETSHOP</title>
-	<link rel="stylesheet" type="text/css" href="../css/estilo.css">
-	<link rel="icon" href="../img/icone.ico">
+	<link rel="stylesheet" type="text/css" href="./css/estilo.css">
+	<link rel="icon" href="./img/icone.ico">
 </head>
 <body>
--- pages/login.php
+++ pages/login.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Login</title>
-</head>
-<body>
+<?php
+require_once 'cabecalho.php';
+?>
+<form action="/login" method="POST" class="login">
+	<h1>Login</h1>
+	<p>Digite o nome de usuário:</p>
+	<p><input type="text" name="nome" size="30" maxlength="30" pattern="[a-zA-Z_\s0-9]{1,30}" required></p>
+	<p>Senha:</p>
+	<p><input type="password" name="senha" size="10" maxlength="10" pattern="[a-zA-Z0-9_]{1,10}" required></p>
+	<p><input type="submit" name="botao" value="Logar">	
+</form>
 
 </body>
 </html>
