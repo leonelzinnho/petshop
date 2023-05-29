36e0d41b08a6a60aed823eab3a166127ec44a74e
ggfrancagg <gfrancagg@gmail.com>
2023-05-29 14:24:25 -0300

    Login

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Home</title>
-</head>
-<body>
+<?php
+require_once 'cabecalho.php';
 
+require_once './persistence/UsuarioPA.php';
+$usuarioPA=new UsuarioPA();
+$tipo=$usuarioPA->retornaTipo($_COOKIE['usuario']);
+if (!$tipo) {
+	echo "<h2>Tente logar novamente!</h2>";
+	echo "<a href='/login'>Login</a>";
+}else{
+	if ($tipo==1) {
+?>
+	<div id="topo">
+		<div id="logo">
+			<img src="../img/logo.jpg">
+		</div>
+		<div id="menu">
+			<ul class="nav">
+				<li>Cadastrar
+					<ol>
+						<li><a href="/cadastrar/cliente" target="janela">Cliente</a></li>
+						
+
+
+
+<?php
+	}
+
+}
+
+?>
 </body>
 </html>
