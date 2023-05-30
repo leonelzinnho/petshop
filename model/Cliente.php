9fe1f71999d27b92919ec0ef7611a79c4ba8178e
ggfrancagg <gfrancagg@gmail.com>
2023-05-30 14:29:19 -0300

    Class Cliente

---
+++ model/Cliente.php
@@ -0,0 +1,85 @@
+<?php
+
+class Cliente{
+
+	private $id;
+	private $cpf;
+	private $nome;
+	private $nascimento;
+	private $telefone;
+	private $rua;
+	private $bairro;
+	private $cidade; 
+	private $estado;
+	private $email;
+
+	public function setId($id)
+	{
+		$this->id=$id;
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setCpf($cpf)
+	{
+		$this->cpf=$cpf;
+	}
+
+	public function getCpf(){
+		return $this->cpf;
+	}
+
+	public function setNome($nome)
+	{
+		$this->nome=$nome;
+	}
+
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function setNascimento($nascimento)
+	{
+		$this->nascimento=$nascimento;
+	}
+
+	public function getNascimento(){
+		return $this->nascimento;
+	}
+
+	public function setTelefone($telefone)
+	{
+		$this->telefone=$telefone;
+	}
+
+	public function getTelefone(){
+		return $this->telefone;
+	}
+
+	public function setRua($rua)
+	{
+		$this->rua=$rua;
+	}
+
+	public function getRua(){
+		return $this->rua;
+	}
+
+	public function setBairro($bairro)
+	{
+		$this->bairro=$bairro;
+	}
+
+	public function getBairro(){
+		return $this->bairro;
+	}
+
+	
+
+
+
+}
+
+?>
